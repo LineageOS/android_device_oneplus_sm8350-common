09ae1b396a98c1d2e14f2c0eaa792e55e106263b
Will McVicker <willmcvicker@google.com>
2020-03-19 10:38:31 -0700

    sm8350-common: Add support for vendor_boot
    This enables boot header v3 which creates a vendor_boot images that
    holds the vendor kernel modules.
    
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    Fixes: 151949371
    Test: boot, verify vendor_boot ramdisk is loaded, check lsmod
    Change-Id: Ia1b7df306d3a9f0e2f1f3ec8e7c2547da0b07596

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -5,7 +5,17 @@ LOCAL_MODULE           := fstab.qcom
 LOCAL_MODULE_TAGS      := optional
 LOCAL_MODULE_CLASS     := ETC
 LOCAL_SRC_FILES        := etc/fstab.qcom
+LOCAL_REQUIRED_MODULES := fstab.qcom.first_stage_ramdisk
 LOCAL_MODULE_PATH      := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE           := fstab.qcom.first_stage_ramdisk
+LOCAL_MODULE_STEM      := fstab.qcom
+LOCAL_MODULE_TAGS      := optional
+LOCAL_MODULE_CLASS     := ETC
+LOCAL_SRC_FILES        := etc/fstab.qcom
+LOCAL_MODULE_PATH      := $(TARGET_VENDOR_RAMDISK_OUT)/first_stage_ramdisk
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
