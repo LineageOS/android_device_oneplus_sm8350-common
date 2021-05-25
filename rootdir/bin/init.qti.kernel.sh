b8e62fc2c502d492e58741f59bcf24d6e38bf5e0
Bruno Martins <bgcngm@gmail.com>
2021-05-25 23:59:05 +0100

    sm8350-common: Update init scripts from OOS
    Change-Id: Ia4e798ae3c5cbdb7246669b833c75e7058efa523

--- rootdir/bin/init.qti.kernel.sh
+++ rootdir/bin/init.qti.kernel.sh
@@ -1,6 +1,10 @@
 #! /vendor/bin/sh
-
-# Copyright (c) 2013, The Linux Foundation. All rights reserved.
+#=============================================================================
+# Copyright (c) 2019-2020 Qualcomm Technologies, Inc.
+# All Rights Reserved.
+# Confidential and Proprietary - Qualcomm Technologies, Inc.
+#
+# Copyright (c) 2009-2012, 2014-2019, The Linux Foundation. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -9,7 +13,7 @@
 #     * Redistributions in binary form must reproduce the above copyright
 #       notice, this list of conditions and the following disclaimer in the
 #       documentation and/or other materials provided with the distribution.
-#     * Neither the name of Linux Foundation nor
+#     * Neither the name of The Linux Foundation nor
 #       the names of its contributors may be used to endorse or promote
 #       products derived from this software without specific prior written
 #       permission.
@@ -25,10 +29,20 @@
 # WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
+#=============================================================================
 
-baseband=`getprop ro.baseband`
-if [ "$baseband" = "mdm" ] || [ "$baseband" = "mdm2" ]; then
-	start vendor.mdm_helper
-fi
+verify_pasr_support()
+{
+         if [ -d /sys/kernel/mem-offline ]; then
+                setprop vendor.pasr.enabled true
+         fi
+}
 
+start_msm_irqbalance()
+{
+         if [ -f /vendor/bin/msm_irqbalance ]; then
+                start vendor.msm_irqbalance
+         fi
+}
+start_msm_irqbalance
+verify_pasr_support
