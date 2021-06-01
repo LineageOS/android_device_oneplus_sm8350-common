09c59cd8d90e2a1dff6f22e685d23ce187f8ae2c
Bruno Martins <bgcngm@gmail.com>
2021-06-01 17:24:50 +0100

    sm8350-common: Remove useless or unused init scripts
    Change-Id: I579319d620b14aab0decd49587d58a20262576ae

--- common.mk
+++ common.mk
@@ -201,9 +201,7 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     ftm_power_config.sh \
     init.class_main.sh \
-    init.crda.sh \
     init.cust.rc \
-    init.mdm.sh \
     init.oem.debug.rc \
     init.oem.minidump.sdx55m.rc \
     init.oem.rc \
@@ -211,19 +209,16 @@ PRODUCT_PACKAGES += \
     init.oem_ftm.rc \
     init.oplus_chg.sh \
     init.qcom.class_core.sh \
-    init.qcom.coex.sh \
     init.qcom.early_boot.sh \
     init.qcom.efs.sync.sh \
     init.qcom.factory.rc \
     init.qcom.post_boot.sh \
     init.qcom.rc \
-    init.qcom.sdio.sh \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
     init.qti.chg_policy.sh \
     init.qti.dcvs.sh \
-    init.qti.ims.sh \
     init.qti.qcv.sh \
     init.recovery.qcom.rc \
     init.target.rc \
