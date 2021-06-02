3a15c25272fa7f72cd17c0fca9f4db6657146e96
LuK1337 <priv.luk@gmail.com>
2021-06-02 17:51:19 +0200

    sm8350-common: No LTO for me, please.
    Change-Id: I6c4ca01ec99b79b985446b74cd70314235295fe8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -119,8 +119,7 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     BRAND_SHOW_FLAG=oneplus \
-    DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc \
-    LLVM=1
+    DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8350
 TARGET_KERNEL_CONFIG := vendor/lahaina-qgki_defconfig
 
