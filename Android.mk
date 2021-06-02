c56b1c9a6f84f8fc173f5788c9baf9178a5ff127
LuK1337 <priv.luk@gmail.com>
2021-06-02 20:27:21 +0200

    sm8350-common: Use local WCNSS_qcom_cfg.ini
    Change-Id: Iecca4f981fdf838020d74cbabc57088b1ac1a18c

--- Android.mk
+++ Android.mk
@@ -94,7 +94,7 @@ WIFI_FIRMWARE_SYMLINKS := $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld
 $(WIFI_FIRMWARE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Creating wifi firmware symlinks: $@"
 	@mkdir -p $@
-	$(hide) ln -sf /mnt/vendor/persist/WCNSS_qcom_cfg.ini $@/WCNSS_qcom_cfg.ini
+	$(hide) ln -sf /vendor/etc/wifi/WCNSS_qcom_cfg.ini $@/WCNSS_qcom_cfg.ini
 	$(hide) ln -sf /mnt/vendor/persist/wlan_mac.bin $@/wlan_mac.bin
 
 VENDOR_RAMDISK_KERNEL_MODULES_LIST := $(strip $(shell cat $(COMMON_PATH)/modules.load.recovery))
