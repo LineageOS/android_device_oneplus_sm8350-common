c5dc20632fc86aed4b720d447926c0d097c092e8
Han Wang <416810799@qq.com>
2021-06-05 10:53:45 +0200

    sm8350-common: Unset PRODUCT_USE_PRODUCT_VNDK_OVERRIDE
     * This is not needed if PRODUCT_SHIPPING_API_LEVEL is equal
       or greater than 29, which is true for sm8350 series.
    
    Change-Id: I5b3fa65dfaae20c788a96a6a22d4016ddfb19983

--- common.mk
+++ common.mk
@@ -43,9 +43,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
-# VNDK
-PRODUCT_USE_PRODUCT_VNDK_OVERRIDE := true
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
