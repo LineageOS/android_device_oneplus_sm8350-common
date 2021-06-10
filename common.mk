8f14d1f3397d68b2cc67c951e41a4cf2d3a65bdf
LuK1337 <priv.luk@gmail.com>
2021-06-10 09:24:34 +0200

    sm8350-common: Build android.hardware.thermal@2.0-service.qti
    As seen on stock ROM.
    
    Change-Id: I950ab50ded7b3c1164ff88ab18d7f1e3365a6086

--- common.mk
+++ common.mk
@@ -468,6 +468,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti
+
 # Touch
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.oneplus_lahaina
