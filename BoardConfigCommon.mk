43c1a8c24fcb7aa612be7e5e3bd3162abfbf6411
Bruno Martins <bgcngm@gmail.com>
2021-07-21 13:39:40 +0100

    sm8350-common: Update blobs from OOS 11.2.8.8.LE15BA
    Change-Id: I7a773dbab1bfe8cd993380fb88a0b5160066de6d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -186,7 +186,7 @@ CUSTOM_APNS_FILE := $(COMMON_PATH)/configs/apns-conf.xml
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2021-05-01
+VENDOR_SECURITY_PATCH := 2021-07-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
