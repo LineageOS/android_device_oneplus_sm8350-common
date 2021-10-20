bf78c13bdc3b59bbd4bb4a8f51eeb50f0f6d4b98
LuK1337 <priv.luk@gmail.com>
2021-10-20 19:39:29 +0200

    sm8350-common: Adjust for recent tri state key changes
    tri-state-key package has been integrated into KeyHandler app, and
    KeyHandler itself no longer needs to be added to lineage-sdk overlays.
    
    Change-Id: I13b8f05e8e0b036c9303a01ec0c7e2d99b6756f7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -197,6 +197,7 @@ VENDOR_SECURITY_PATCH := 2021-09-01
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/private
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Verified Boot
