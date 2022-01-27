2adf8af1aa8548bd5b44b5bd38b94063a0a94fba
Bruno Martins <bgcngm@gmail.com>
2022-01-27 16:48:31 +0000

    sm8350-common: Update to Android 12 blobs
    Change-Id: Ia412660d84825b47c2f272f89baf73cf5981879c

--- extract-files.sh
+++ extract-files.sh
@@ -58,14 +58,6 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
-function blob_fixup() {
-    case "${1}" in
-        system_ext/lib64/libwfdnative.so)
-            sed -i "s/android.hidl.base@1.0.so/libhidlbase.so\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00/" "${2}"
-            ;;
-    esac
-}
-
 if [ -z "${ONLY_TARGET}" ]; then
     # Initialize the helper for common device
     setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
