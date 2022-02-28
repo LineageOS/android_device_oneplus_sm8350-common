70fe34adb3e25e9cda37643c934896bf5fe1cd13
Michael Bestas <mkbestas@lineageos.org>
2022-02-28 22:51:41 +0200

    sm8350-common: bootctrl: Remove bootctrl.lahaina target
    * This is no longer used after switching to QTI 1.1 impl
    
    Change-Id: Id770b77da62c3b16eaea83cec366b299b1ea49d2

--- common.mk
+++ common.mk
@@ -109,9 +109,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-impl-qti \
     android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service \
-    bootctrl.lahaina \
-    bootctrl.lahaina.recovery
+    android.hardware.boot@1.1-service
 
 # Camera
 PRODUCT_PACKAGES += \
