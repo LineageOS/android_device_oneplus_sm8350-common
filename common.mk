26844cc498606d3bd093d6bf101a855682252587
althafvly <althafvly@gmail.com>
2022-03-24 17:18:43 +0530

    sm8350-common: Inherit launch_with_vendor_ramdisk for vAB
    - sm8350 devices have a vendor_boot partition
    
    Change-Id: I5c344890bc12ee769e2f27369a4151b469271a51

--- common.mk
+++ common.mk
@@ -5,7 +5,7 @@
 #
 
 # A/B
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
