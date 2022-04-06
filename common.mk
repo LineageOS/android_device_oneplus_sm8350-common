b8992a62d5901a8a89d8abd3eea27dab50ba9b2d
Albert Tang <tangalbert919@yahoo.com>
2022-04-05 21:22:24 -0500

    sm8350-common: Add thermal-engine.conf
    Taken from AOSPA/android_device_oneplus_oneplus9.
    
    They are not using thermal daemon from OOS, and neither are we,
    so let's just do exactly what they are doing.
    
    Change-Id: I68f0ea53a8ff501bacf9eefd2fd7fdb30672b691

--- common.mk
+++ common.mk
@@ -487,6 +487,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/thermal/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
 # Touch
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.oneplus
