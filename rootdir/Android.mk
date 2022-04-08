6a1ad1fa44eeec0ac89a52800ff6f29952014c37
Albert Tang <tangalbert919@yahoo.com>
2022-04-07 21:10:26 -0500

    sm8350-common: rootdir: Update from ColorOS 12
    Change-Id: I4dcbd867316cb9fe6ef35a16454b85629fb7057f

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -43,11 +43,59 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.kernel.post_boot-lahaina.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := bin/init.kernel.post_boot-lahaina.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.oplus_chg.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := bin/init.oplus_chg.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.oplus.bsp_debug.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := bin/init.oplus.bsp_debug.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ODM)/bin
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.oplus.bsp_us_dump.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := bin/init.oplus.bsp_us_dump.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ODM)/bin
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.oplus.btChangePermission.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := bin/init.oplus.btChangePermission.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ODM)/bin
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.oplus.fingerprints.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := bin/init.oplus.fingerprints.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ODM)/bin
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.oplus.wifi.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := bin/init.oplus.wifi.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ODM)/bin
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -266,4 +314,36 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := ueventd.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.oppo.reserve.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.oppo.reserve.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ODM)/etc/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.oplus.display.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.oplus.display.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ODM)/etc/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.oplus.sensor.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.oplus.sensor.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ODM)/etc/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.oplus.wifi.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.oplus.wifi.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ODM)/etc/init/hw
 include $(BUILD_PREBUILT)
