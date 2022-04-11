5d0ec3fa87cd16ec350bfb0cb1c0936fbc82cdcb
Alexander Koskovich <akoskovich@pm.me>
2022-04-11 11:41:43 -0700

    sm8350-common: init: Use the same PID/VIDs from OOS
    Change-Id: Ieaa52f748c75272b80fa6f3e7018cdb71e3a2f7f

--- common.mk
+++ common.mk
@@ -308,6 +308,7 @@ PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.oplus.display.rc \
     init.oplus.sensors.rc \
+    init.oplus.usb.rc \
     init.qcom.early_boot.sh \
     init.qcom.rc \
     init.qcom.recovery.rc \
