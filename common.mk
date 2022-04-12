6e3876b490ed5bece895c83c3b6d47c9144cc2db
pjgowtham <pjgowtham@gmail.com>
2022-04-12 08:58:28 +0530

    sm8350-common: Switch to oplus fingerprint
    Change-Id: I9e618a567f59f7c800301f40de2ee6716a8d8dc1

--- common.mk
+++ common.mk
@@ -273,7 +273,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.oplus
+    android.hardware.biometrics.fingerprint@2.3-service.oplus \
+    vendor.oplus.hardware.biometrics.fingerprint@2.1
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
