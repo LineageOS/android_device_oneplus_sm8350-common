c39fc3fb549ae20514188c78ec3d44e421a1ce9e
Alexander Koskovich <akoskovich@pm.me>
2022-04-12 05:00:05 -0700

    sm8350-common: Pick up USB configurations from QCOM
     * If we want to override anything in particular, e.g.
       PID/VIDs we will create a seperate script named
       'init.oplus.usb.rc'.
    
    Change-Id: I83882d8992eb367211871130c163f4ca5689fcd4

--- common.mk
+++ common.mk
@@ -307,6 +307,8 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qcom.recovery.rc \
     init.qcom.sh \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh \
     init.target.rc \
     ueventd.qcom.rc
 
@@ -457,6 +459,9 @@ PRODUCT_PACKAGES_DEBUG += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/usb/etc
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
