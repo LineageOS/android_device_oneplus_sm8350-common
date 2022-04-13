7684aaac15f76fdcc340271ca2b4768c315ce3c2
Alexander Koskovich <akoskovich@pm.me>
2022-04-12 17:16:55 -0700

    sm8350-common: Sync properties with OOS
    Change-Id: I7b53d83d9acd46fd91f842f552338952a95f86d3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -83,7 +83,7 @@ TARGET_BOARD_PLATFORM := lahaina
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
-TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
+TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
 # A/B
