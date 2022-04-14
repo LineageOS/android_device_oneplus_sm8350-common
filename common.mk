72897aa00d5b606723b6e99689a6d818da3e33d1
Alexander Koskovich <akoskovich@pm.me>
2022-04-13 20:30:41 -0700

    sm8350-common: overlay: Atomize
    Change-Id: I972d46e1dafb03b42249193fd033e91c4e3b3bc5

--- common.mk
+++ common.mk
@@ -34,7 +34,6 @@ $(call inherit-product, vendor/oneplus/sm8350-common/sm8350-common-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
