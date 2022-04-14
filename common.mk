b54776adf66fbcb0a12e454e775c7a93cdbeaf2f
Alexander Koskovich <akoskovich@pm.me>
2022-04-13 20:41:13 -0700

    sm8350-common: Import cleaned QSSI RROs from LE2123_11.C.47
     * Remove anything that the value is already the default,
       or deprecated.
    
    Change-Id: I597a93b580754a7fde51d051accfffca54c22482

--- common.mk
+++ common.mk
@@ -37,6 +37,12 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_PACKAGES += \
+    CarrierConfigResCommon \
+    FrameworksResCommon \
+    SystemUIResCommon \
+    TelephonyResCommon \
+    WifiResCommon
 
 # Permissions
 PRODUCT_COPY_FILES += \
