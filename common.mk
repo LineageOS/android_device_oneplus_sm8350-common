3bdfc62944b18bb228bbefb16c2e93b4b41af779
Alexander Koskovich <akoskovich@pm.me>
2022-04-17 15:10:18 -0700

    sm8350-common: Update WCNSS configuration from LE2125_11.C.48
     * OnePlus reads MAC address from QMI DMS now.
    
     * Since that was the only custom change, move back
       to proprietary files.
    
    This commit is a partial revert of 83d053860901aaa5b6fdb54c6160aac817c6806b.
    
    Change-Id: I9e89def5258f9085e7169a1a6753cbdabd1d548c

--- common.mk
+++ common.mk
@@ -491,9 +491,6 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
-
 # WiFi Display
 PRODUCT_PACKAGES += \
     libnl \
