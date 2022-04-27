a9cfa22e147d60f513e862eae37411103f79e02d
Alexander Koskovich <akoskovich@pm.me>
2022-04-26 21:15:51 -0700

    sm8350-common: Drop console cmdline
     * '115200n8' is only used by sdmshrike for stdout-path.
    
    Change-Id: I4ff5686b5c9f9689a4422d9fcb353cf6edbb6954

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -102,7 +102,6 @@ BOARD_KERNEL_CMDLINE := \
     androidboot.memcg=1 \
     androidboot.usbcontroller=a600000.dwc3 \
     cgroup.memory=nokmem,nosocket \
-    console=115200n8 \
     loop.max_part=7 \
     lpm_levels.sleep_disabled=1 \
     msm_rtb.filter=0x237 \
