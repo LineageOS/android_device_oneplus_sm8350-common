93cc3a887d67237c192c1b8a90a9bc52d978ec4f
Alexander Koskovich <akoskovich@pm.me>
2022-04-27 16:51:47 -0700

    sm8350-common: Add missing dependencies of psiclient
     * F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.qti.psiclient@1.0-service": library "libpsi.so" not found: needed by main executable
       F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.qti.psiclient@1.0-service": library "libpasrutils.so" not found: needed by main executable
    
    Change-Id: Ie18fbab15c71dc1df31b8c34b9f1bbfbfbc14642

--- common.mk
+++ common.mk
@@ -362,6 +362,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     android.hardware.power@1.2.vendor \
+    libpsi.vendor \
     vendor.qti.hardware.perf@2.2.vendor
 
 # PowerShare
