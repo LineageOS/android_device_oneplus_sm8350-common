657b6fdf4d70b58e58f83c76504bfcad98c6b33f
pjgowtham <pjgowtham@gmail.com>
2022-05-19 11:41:13 +0530

    sm8350-common: Copy configs and scripts for yupik
    * Haven't added shima because there are no oplus shima devices thus far
    
    Change-Id: I30061d7291622f37f09a160bf2f394b9cb8a26fd

--- extract-files.sh
+++ extract-files.sh
@@ -69,7 +69,7 @@ function blob_fixup() {
         system_ext/lib64/libwfdnative.so)
             sed -i "s/android.hidl.base@1.0.so/libhidlbase.so\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00/" "${2}"
             ;;
-        vendor/etc/media_lahaina/video_system_specs.json)
+        vendor/etc/media_*/video_system_specs.json)
             sed -i "/max_retry_alloc_output_timeout/ s/1000/0/" "${2}"
             ;;
         vendor/lib/libgui1_vendor.so)
