93ee9de1cc83bc0d202ccd6b28384dcc2af80c58
Alexander Koskovich <akoskovich@pm.me>
2022-05-30 14:08:30 -0700

    sm8350-common: Update from OOS 11.C.61
     * Drop ro.soc.model from vendor.prop as it's set by
       init.qti.qcv.rc now.
    
    Change-Id: I076603078169bed08035bb7e6311b649b9c1fdc0

--- extract-files.sh
+++ extract-files.sh
@@ -76,10 +76,10 @@ function blob_fixup() {
             "${PATCHELF}" --replace-needed "libui.so" "libui-v30.so" "${2}"
             ;;
         vendor/lib64/hw/com.qti.chi.override.so)
-            "${SIGSCAN}" -p "C0 6D 05 94" -P "1F 20 03 D5" -f "${2}"
+            "${SIGSCAN}" -p "05 B8 05 94" -P "1F 20 03 D5" -f "${2}"
             ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
-            "${SIGSCAN}" -p "3F 0A 00 94" -P "1F 20 03 D5" -f "${2}"
+            "${SIGSCAN}" -p "23 0B 00 94" -P "1F 20 03 D5" -f "${2}"
             ;;
     esac
 }
