4dde55c0b94eb77c95c5e56171235fb105070dc4
Alexander Koskovich <akoskovich@pm.me>
2022-06-06 09:13:49 -0700

    sm8350-common: Enable LLVM integrated assembler
     * Disabled by default even with LLVM=1, but this is
       another GCC dependency we can remove. Google has
       been doing this for a while now.
    
       https://android.googlesource.com/kernel/msm/+/b009f0a1a
    
    Change-Id: I1ca23607682299182da0a6e14594ba2e2de00a4a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -108,7 +108,8 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc \
-    LLVM=1
+    LLVM=1 \
+    LLVM_IAS=1
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8350
 TARGET_KERNEL_CONFIG := vendor/lahaina-qgki_defconfig
 
