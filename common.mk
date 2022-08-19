ffefa6008e3e04e628aedb4bb002b4763777a5d9
Alexander Koskovich <akoskovich@pm.me>
2022-08-19 09:40:56 -0700

    sm8350-common: Restore component overrides for BT AOSP
    This partially reverts commit 369c86860e539a235391468af3d0d2b7dbe8a7e7.
    
     * Required for AOSP Bluetooth on Lineage, but not GSI
       for whatever reason.
    
    Change-Id: Ibf732ba4018cfc0af87bd15f7f309aac14fa556b

--- common.mk
+++ common.mk
@@ -99,6 +99,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
