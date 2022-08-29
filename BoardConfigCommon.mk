5030e4164cee51e27787bc985bb121dfd4a9ffa5
Edwin Moquete <edwinmmoquete@gmail.com>
2022-08-29 01:37:22 +0000

    sm8350-common: Switch to legacy sepolicy_vndr
    Change-Id: I13229c3057bf32c81b0fdace075f0791cb8ad380

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -172,7 +172,7 @@ BOOT_SECURITY_PATCH := 2022-08-05
 VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # SEPolicy
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 include hardware/oplus/sepolicy/qti/SEPolicy.mk
 
 # Verified Boot
