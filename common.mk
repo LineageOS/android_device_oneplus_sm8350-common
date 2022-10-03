75f1ed22a72af79c4fb37af3e5539488af8b5d7c
Michael Bestas <mkbestas@lineageos.org>
2022-10-03 03:49:23 +0300

    fixup! sm8350-common: Build fastboot custom HAL
    Change-Id: If8b60462306f2f92789f57749e9c6dcb81e2d096

--- common.mk
+++ common.mk
@@ -170,7 +170,7 @@ PRODUCT_PACKAGES += \
 
 # Fastboot
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.1-impl-custom \
+    android.hardware.fastboot@1.1-impl.custom \
     fastbootd
 
 # Fingerprint
