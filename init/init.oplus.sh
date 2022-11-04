8d7928f1211ab96d45836e8bfc1f1504e6c7efb3
LuK1337 <priv.luk@gmail.com>
2022-11-04 16:22:29 +0100

    sm8150-common: Go back to old dsds detection
    New one can result in false positives ;(
    
    Change-Id: I5b47acaa89b09079acba879e292c1a6274d63996

--- init/init.oplus.sh
+++ init/init.oplus.sh
@@ -5,6 +5,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# 18821 - 7 Pro
+# 18831 - 7 Pro TMO
+# 18857 - 7
+# 18865 - 7T
+# 19801 - 7T Pro
+# 19863 - 7T TMO
+
 if grep -qE "androidboot.rf_version=(11|13|21)" /proc/cmdline; then
     setprop vendor.radio.multisim.config dsds
 fi
