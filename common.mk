5e28e4b30c1491d18a43c9185c104bd58a7d4d7d
Rohit R Sekhar <merothh@e.email>
2022-11-17 21:27:27 +0530

    sm8350-common: Build init.qti.display_boot scripts
    Change-Id: I8e77710f2490587b811fd6238491a92900c35d34

--- common.mk
+++ common.mk
@@ -117,6 +117,8 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
+    init.qti.display_boot.rc \
+    init.qti.display_boot.sh \
     libmemutils \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service.rc \
