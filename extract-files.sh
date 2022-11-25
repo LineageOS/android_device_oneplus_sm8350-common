f4073942d8810cc693e2fea5fd0b4a8b935b85fa
LuK1337 <priv.luk@gmail.com>
2022-11-25 19:18:37 +0100

    sm8350-common: Update from OOS 11.C.66
    Change-Id: I58c7235e83dfe045f86f38c16acbbc508331b047

--- extract-files.sh
+++ extract-files.sh
@@ -75,9 +75,6 @@ function blob_fixup() {
         vendor/lib/libgui1_vendor.so)
             "${PATCHELF}" --replace-needed "libui.so" "libui-v30.so" "${2}"
             ;;
-        vendor/lib64/hw/com.qti.chi.override.so)
-            "${SIGSCAN}" -p "45 B8 05 94" -P "1F 20 03 D5" -f "${2}"
-            ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
             "${SIGSCAN}" -p "23 0B 00 94" -P "1F 20 03 D5" -f "${2}"
             ;;
