5172e4aafe865a104fec119d40a96c839054d832
pjgowtham <pjgowtham@gmail.com>
2023-02-23 19:18:39 +0530

    sm8350-common: Add support for OnePlus 9RT and Realme GT Master
    Change-Id: I3650defba1a89ab3045bb705eb1edd32e190c8fa

--- Android.mk
+++ Android.mk
@@ -1,12 +1,12 @@
 #
-# Copyright (C) 2021-2022 The LineageOS Project
+# Copyright (C) 2021-2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter lemonade lemonadep,$(TARGET_DEVICE)),)
+ifneq ($(filter lemonade lemonadep lunaa martini,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
