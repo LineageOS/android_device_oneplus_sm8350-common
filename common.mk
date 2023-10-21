f7f504f1e66f1657851595c2f0aa32f74e9e869e
LuK1337 <priv.luk@gmail.com>
2023-10-21 10:07:10 +0200

    sm8350-common: Update libstdc++.vendor target name
    Change-Id: I9fb48d50df7b8b74d19bfb05b3d9c2ca169bf3e2

--- common.mk
+++ common.mk
@@ -59,7 +59,7 @@ PRODUCT_PACKAGES += \
     libsndmonitor \
     libspkrprot \
     libssrec \
-    libstdc++.vendor \
+    libstdc++_vendor \
     libvolumelistener \
     sound_trigger.primary.lahaina:32
 
--- extract-files.sh
+++ extract-files.sh
@@ -81,6 +81,9 @@ function blob_fixup() {
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
             "${SIGSCAN}" -p "23 0B 00 94" -P "1F 20 03 D5" -f "${2}"
             ;;
+        odm/lib/liblvimfs_wrapper.so|odm/lib64/libCOppLceTonemapAPI.so|odm/lib64/libaps_frame_registration.so|vendor/lib64/libalsc.so)
+            "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
+            ;;
     esac
 }
 
