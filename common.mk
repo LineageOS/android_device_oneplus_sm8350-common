74a17a0774f02294d39db7ef6d28142a3fc4602d
pjgowtham <pjgowtham@gmail.com>
2024-02-18 22:44:59 +0530

    sm8350-common: Build qti health AIDL for recovery
    Change-Id: I344c0287bd3ae4b31ee53b91036e7a60a0185c55

--- common.mk
+++ common.mk
@@ -165,7 +165,8 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.qti
+    android.hardware.health-service.qti \
+    android.hardware.health-service.qti_recovery
 
 # Hotword enrollment
 PRODUCT_COPY_FILES += \
