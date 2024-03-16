0bf68f1a17d42297347145ded3c1c545de40e13d
Albert Tang <tangalbert919@yahoo.com>
2024-03-16 16:46:22 -0500

    sm8350-common: Update to Android 14 (LE2125_11.H.16)
    Change-Id: I27180a420547b933c9bcbcaef672cab50be06945

--- common.mk
+++ common.mk
@@ -108,6 +108,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.stats@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     vendor.qti.hardware.camera.postproc@1.0.vendor
