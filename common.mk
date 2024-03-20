4f888d6ff2c056471a80989c2fa9f8961b4a7b8d
basamaryan <basam.aryan@gmail.com>
2024-03-20 01:16:12 -0400

    sm8350-common: Don't explicitly include updatable_apex.mk
    It is inherited by base_system.mk as of QPR2
    
    Change-Id: I020a05b233f61ea28d03199cbc76051e6e97e1dc

--- common.mk
+++ common.mk
@@ -28,9 +28,6 @@ PRODUCT_PACKAGES += \
     AntHalService-Soong \
     com.dsi.ant@1.0.vendor
 
-# APEX
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Atrace
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service
