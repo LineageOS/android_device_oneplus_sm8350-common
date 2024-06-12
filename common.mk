9bef029e8c1d81d5fc3622f8381e299edcb3c0f5
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-12 22:41:36 +0800

    sm8350-common: Setup DeviceAsWebcam
    Change-Id: I448334e51a577702007442ff6eaa27b3d78f02ae

--- common.mk
+++ common.mk
@@ -221,6 +221,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
+    DeviceAsWebcamResTarget \
     FrameworksResTarget \
     OPlusFrameworksResCommon \
     OPlusSettingsResCommon \
