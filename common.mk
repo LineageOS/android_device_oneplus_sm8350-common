f01465beab757f6776c788d6f18762f79008b6a8
Chirayu Desai <chirayudesai1@gmail.com>
2024-06-13 22:02:20 +0530

    sm8350-common: Disable UFFD GC
    Unable to determine UFFD GC flag for kernel version "5.4.268-qgki".
    You can fix this by explicitly setting PRODUCT_ENABLE_UFFD_GC to "true" or
    "false" based on the kernel version.
    1. Set PRODUCT_ENABLE_UFFD_GC to "true" if the kernel supports userfaultfd(2)
       and MREMAP_DONTUNMAP.
    2. Set PRODUCT_ENABLE_UFFD_GC to "false" otherwise.
    
    Change-Id: Ie77d9a86a7fe94e4cade1868ffea27194f724e03

--- common.mk
+++ common.mk
@@ -239,6 +239,9 @@ PRODUCT_PACKAGES += \
     ueventd.oplus.rc \
     ueventd.qcom.rc
 
+# Kernel
+PRODUCT_ENABLE_UFFD_GC := false
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
