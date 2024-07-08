bd18a70d4d452557f96c2cb13ba73eeb20a52a0f
Michael Bestas <mkbestas@lineageos.org>
2024-07-08 08:56:25 +0300

    sm8350-common: Remove media_codecs_google_c2*
    AOSP codecs can be used by default and it is not necessary to include
    them device side.
    
    Change-Id: I8227b06f0466b614f12fa9008402b92461ea3dd0

--- common.mk
+++ common.mk
@@ -257,11 +257,6 @@ PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.1-service.oplus
 
 # Media
-PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_google_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
-
 PRODUCT_PACKAGES += \
     libavservices_minijail \
     libavservices_minijail.vendor \
