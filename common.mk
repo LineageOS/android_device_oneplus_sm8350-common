e873f3925c7f65ae3881efef303ca3e960c0eef9
LuK1337 <priv.luk@gmail.com>
2024-10-20 10:43:21 +0200

    sm8350-common: Unset BOARD_USES_ALSA_AUDIO
    Lets us skip audio HAL module.
    
    Change-Id: Ibe064be7db15b0b0c2724a26f7a6abe400bca010

--- common.mk
+++ common.mk
@@ -43,7 +43,6 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
     android.hardware.soundtrigger@2.3-impl \
     audio.bluetooth.default \
-    audio.primary.lahaina \
     audio.r_submix.default \
     audio.usb.default \
     audioadsprpcd \
