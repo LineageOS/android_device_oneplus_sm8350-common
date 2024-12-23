336d0d39acf5adca3fed9778a3a985c4d6e7b423
pjgowtham <pjgowtham@gmail.com>
2024-12-23 19:59:20 +0530

    sm8350-common: Switch to oss audio HAL
    LVIMFS/LVACFS feature was preventing us from using oss audio hal.
    Now that it is sorted out, let's just switch to oss audio HAL.
    
    This reverts commit 7768a002ea579d122ca067c3700a8ddc920794eb.
    
    Change-Id: I44486535b2fa765d978203c696f5adfd5ca0b46a

--- common.mk
+++ common.mk
@@ -42,6 +42,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
     android.hardware.soundtrigger@2.3-impl \
     audio.bluetooth.default \
+    audio.primary.lahaina \
     audio.r_submix.default \
     audio.usb.default \
     audioadsprpcd \
