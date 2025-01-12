6182930351c34ebcaddf27e7c36ce0412eb56503
Bruno Martins <bgcngm@gmail.com>
2025-01-12 09:05:07 +0000

    sm8350-common: Drop all Android makefiles inclusion within the tree
    Change-Id: I33a163d7243fe1203538d6e37a907acd3976f1d4

--- Android.mk
+++ Android.mk
@@ -1,13 +1,10 @@
 #
-# Copyright (C) 2021-2024 The LineageOS Project
+# Copyright (C) 2021-2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := $(call my-dir)
-
 ifneq ($(filter lemonade lemonadep lunaa martini,$(TARGET_DEVICE)),)
-include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
