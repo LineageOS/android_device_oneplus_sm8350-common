d088ea0ee05314863d00d58b6c841c57358ab851
Bruno Martins <bgcngm@gmail.com>
2025-02-06 21:32:31 +0000

    sm8350-common: Use aidl sensors multihal
    Change-Id: I21da3da39e9726dca693d6bcbe42e93be861f7c7

--- common.mk
+++ common.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-2023 The LineageOS Project
+# Copyright (C) 2021-2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -247,7 +247,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors-service.multihal \ \
     sensors.oplus
 
 PRODUCT_COPY_FILES += \
