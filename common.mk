fe9be97c3b14d6152914832d13089cf168bcb7dc
LuK1337 <priv.luk@gmail.com>
2025-02-08 12:16:26 +0100

    sm8350-common: Reenable WiFi Aware
    This partially reverts commit 98080a2194cdf19a9fa8178a8bc2215cf9a66cad.
    
    Test: run CtsVerifier tool
    Change-Id: Ibe8c64d2ba95326594e3103c3cff4c03153e11b9

--- common.mk
+++ common.mk
@@ -362,6 +362,7 @@ PRODUCT_PACKAGES += \
     wpa_supplicant.conf
 
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.wifi.aware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.aware.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
