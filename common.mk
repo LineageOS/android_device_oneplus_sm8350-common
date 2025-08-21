14377243da386244bc074ff5af77329bf8e24c16
LuK1337 <priv.luk@gmail.com>
2025-08-21 19:01:20 +0200

    sm8350-common: Migrate to AIDL camera provider
    Change-Id: I290ecfd9760d6428fe64f137974c353846c0e35d

--- common.mk
+++ common.mk
@@ -91,8 +91,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64
+    android.hardware.camera.provider-service.lineage
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
