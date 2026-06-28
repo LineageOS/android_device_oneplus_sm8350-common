6d4b4fdbe278b2259f2f23f90e09a83f58721d3b
Bruno Martins <bgcngm@gmail.com>
2026-06-28 16:02:42 +0100

    sm8350-common: Drop recovery variant of init.oplus.hw.rc
    Change-Id: Iea98e95de70a47114682d30d5c4d5a6e732eb091

--- common.mk
+++ common.mk
@@ -179,7 +179,6 @@ PRODUCT_PACKAGES += \
     fstab.default.vendor_ramdisk \
     init.class_main.sh \
     init.oplus.hw.rc \
-    init.oplus.hw.rc.recovery \
     init.oplus.rc \
     init.oplus.sh \
     init.qcom.early_boot.sh \
